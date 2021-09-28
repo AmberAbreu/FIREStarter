e85c7acd87270676900ed5269985892528d63e3b
Amber Abreu <ambermabreu@gmail.com>
2021-09-28 15:41:30 -0400

    Feature: Expenses get routes

--- server/api/expenses.js
+++ server/api/expenses.js
@@ -5,56 +5,64 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await Expenses.findAll({
+    const expenses = await Expenses.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
     })
-    res.json(users)
+    res.json(expenses)
   } catch (err) {
     next(err)
   }
 })
 
-router.post('/', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
-    const users = await Expenses.findByPk({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      
-    })
-    res.json(users)
+    const expense = await Expenses.findByPk(req.params.id)
+    if(expense){
+    res.json(expense)
+    }else{
+      next({ message: "some problem occured", status: 404})
+    }
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/', async (req, res, next) => {
-  try {
-    const users = await Expenses.findByPk({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const createdExpense = await Expense.create(req.body)
+    
+    
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
-router.delete('/', async (req, res, next) => {
-  try {
-    const users = await Expenses.findByPk({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
+// router.put('/', async (req, res, next) => {
+//   try {
+//     const users = await Expenses.findByPk({
+//       // explicitly select only the id and username fields - even though
+//       // users' passwords are encrypted, it won't help if we just
+//       // send everything to anyone who asks!
       
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+//     })
+//     res.json(users)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+// router.delete('/', async (req, res, next) => {
+//   try {
+//     const users = await Expenses.findByPk({
+//       // explicitly select only the id and username fields - even though
+//       // users' passwords are encrypted, it won't help if we just
+//       // send everything to anyone who asks!
+      
+//     })
+//     res.json(users)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
--- server/app.js
+++ server/app.js
@@ -1,56 +1,14 @@
 const express = require('express')
-const app = express()
-const port = 3000
-const morgan = require('morgan')
-module.exports = app
-const path = require('path')
+const expenses = require('./api/expenses')
 
-app.use(morgan('dev'))
+require('dotenv').config()
 
-// body parsing middleware
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
 app.use(express.json())
 
-// auth and api routes
-app.use('/auth', require('./auth'))
-app.use('/api', require('./api'))
+app.use('/expenses', expenses)
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
-})
-
-// static file-serving middleware
-app.use(express.static(path.join(__dirname, '..', 'public')))
-
-// any remaining requests with an extension (.js, .css, etc.) send 404
-app.use((req, res, next) => {
-  if (path.extname(req.path).length) {
-    const err = new Error('Not found')
-    err.status = 404
-    next(err)
-  } else {
-    next()
-  }
-})
-
-// sends index.html
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-})
-
-// error handling endware
-app.use((err, req, res, next) => {
-  console.error(err)
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
-
-
-
-// tutorial code:
-// app.get('/', (req, res) => {
-//   res.status(200).send('Hello World!');
-// })
-
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}.`)
-// })
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
