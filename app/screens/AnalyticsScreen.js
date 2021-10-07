f54493e95c2934199cd097e9e64f63e29a7962fc
Amber Abreu <ambermabreu@gmail.com>
2021-10-07 15:10:45 -0400

    Successfully added data from DB to Analytics Screen

--- app/screens/AnalyticsScreen.js
+++ app/screens/AnalyticsScreen.js
@@ -2,20 +2,21 @@ import React, {useEffect} from 'react'
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import { connect } from "react-redux";
 import { getCategories } from '../store/categoriesReducer';
+import {useNavigation,useIsFocused} from '@react-navigation/native'
 
 import {VictoryPie, VictoryBar} from 'victory-native'
 
 import categoriesData from '../dummydata'
 
 
-export function renderChart({navigation}, props) {
+function renderChart( props) {
   const [selectedCategory, setSelectedCategory] = React.useState(null)
+  const navigation = useNavigation()
 
   useEffect(() => {
-    // const unsubscribe = navigation.addListener('focus', () => {
-    //   props.getCategories()
-    // });
-    console.log(props)
+    const unsubscribe = navigation.addListener('focus', () => {
+    props.getCategories()
+     });
     
   }, [])
 
@@ -23,48 +24,48 @@ const windowWidth = Dimensions.get('window').width
 const {categories} = props
 
 
-  // function processCategoryDataToDisplay(){
+  function processCategoryDataToDisplay(){
 
-  //   let chartData = categories.map((item) => {
+    let chartData = categories.map((item) => {
       
-  //     let total = item.expenses.reduce((a, b ) => a + (b.total ), 0)
-  //     return {
-  //       name: item.name,
-  //       y: total,
-  //       expenseCount: item.expenses.length,
-  //       color: item.color,
-  //       id: item.id
-  //     }
-  //   })
+      let total = item.expenses.reduce((a, b ) => a + (b.total ), 0)
+      return {
+        name: item.name,
+        y: total,
+        expenseCount: item.expenses.length,
+        color: item.color,
+        id: item.id
+      }
+    })
 
 
-  //   let filterChartData = chartData.filter(a => a.y > 0)
-  //   let totalExpense = filterChartData.reduce((a,b) => a + (b.y || 0), 0)
-  //   let finalChartData = filterChartData.map((item) => {
-  //     let percentage = (item.y / totalExpense * 100).toFixed(0)
-  //     return {
-  //       label: `${percentage}%`,
-  //       y: Number(item.y),
-  //       expenseCount: item.expenseCount,
-  //       color: item.color,
-  //       name: item.name,
-  //       id: item.id
-  //     }    
-  //   })
-  //   return finalChartData
-  // }
+    let filterChartData = chartData.filter(a => a.y > 0)
+    let totalExpense = filterChartData.reduce((a,b) => a + (b.y || 0), 0)
+    let finalChartData = filterChartData.map((item) => {
+      let percentage = (item.y / totalExpense * 100).toFixed(0)
+      return {
+        label: `${percentage}%`,
+        y: Number(item.y),
+        expenseCount: item.expenseCount,
+        color: item.color,
+        name: item.name,
+        id: item.id
+      }    
+    })
+    return finalChartData
+  }
   function setSelectCategoryByName(name) {
     let category = categories.filter(a => a.name == name)
     setSelectedCategory(category[0])
   }
 
-  // let chartData = processCategoryDataToDisplay()
-  // let colorScales = chartData.map((item) => item.color)
-  // let totalExpenseCount = chartData.reduce((a, b) => a + (b.expenseCount || 0), 0)
+  let chartData = processCategoryDataToDisplay()
+  let colorScales = chartData.map((item) => item.color)
+  let totalExpenseCount = chartData.reduce((a, b) => a + (b.expenseCount || 0), 0)
 
   return (
     <View style={{ alignItems: "center", justifyContent: "center", top: 50 }}>
-      {/* <Text style={{ fontSize: 40 }}>Expenses Data:</Text>
+      <Text style={{ fontSize: 40 }}>Expenses Data:</Text>
       <VictoryPie
         data={chartData}
         colorScale={colorScales}
@@ -109,7 +110,7 @@ const {categories} = props
       <View style={{ position: "absolute", top: "42%", left: "42%" }}>
         <Text style={{ textAlign: "center" }}>{totalExpenseCount}</Text>
         <Text style={{ textAlign: "center" }}>Expenses</Text>
-      </View> */}
+      </View>
 
       
     </View>
@@ -117,11 +118,9 @@ const {categories} = props
 }
 
 
-//const singleData = 
-
-
 
 const mapState = (state) => {
+  console.log("this is state", state)
   return {
     categories: state.categoriesReducer,
   };
