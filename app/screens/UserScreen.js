e4be1738fbd3602cee97771de429986d5c76efa6
Amber Abreu <ambermabreu@gmail.com>
2021-10-30 23:45:59 -0400

    Started UserScreen compnent

--- app/screens/UserScreen.js
+++ app/screens/UserScreen.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 
 const UserScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Periodt pooh</Text>
+      <View
+          style={{
+            backgroundColor: "gray",
+            height: 100,
+            width: 100,
+            borderRadius: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+        </View>
     </SafeAreaView>
   )
 }
