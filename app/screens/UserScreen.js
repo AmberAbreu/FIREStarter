d45a246e37bff8d51752999f9ee0f9d4fa387fa7
Amber Abreu <ambermabreu@gmail.com>
2021-10-31 23:46:16 -0400

    Frontend of UserInfo component complete

--- app/screens/UserScreen.js
+++ app/screens/UserScreen.js
@@ -1,9 +1,37 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native'
+
+
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'User Info',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Finances',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Logout',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
 
 const UserScreen = () => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+
       <View
           style={{
             backgroundColor: "gray",
@@ -15,10 +43,29 @@ const UserScreen = () => {
           }}
         >
         </View>
+        <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   )
 }
 
 export default UserScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 0,
+  },
+  item: {
+    backgroundColor: '#966892',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+})
